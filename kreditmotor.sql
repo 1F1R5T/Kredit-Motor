118470d679820957158fd913d417ce3ecd14481b
Achmad Imannudin <imannaxcrb@gmail.com>
2018-11-14 16:53:07 +0700

    Add files via upload

---
+++ kreditmotor.sql
@@ -0,0 +1,237 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 14, 2018 at 08:36 AM
+-- Server version: 10.1.33-MariaDB
+-- PHP Version: 7.2.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `kreditmotor`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(25) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `username`, `password`) VALUES
+(2, 'akun', '3d342d6e0c524dd57f1728a053eedff3');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barang`
+--
+
+CREATE TABLE `barang` (
+  `id` int(25) NOT NULL,
+  `nama_produk` varchar(255) NOT NULL,
+  `harga` varchar(255) NOT NULL,
+  `kategori` varchar(255) NOT NULL,
+  `stok` int(10) NOT NULL,
+  `kode_barang` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `barang`
+--
+
+INSERT INTO `barang` (`id`, `nama_produk`, `harga`, `kategori`, `stok`, `kode_barang`) VALUES
+(13, 'Revo FIT Galaxy Blue', 'Rp 13,405,000', 'Motor Honda', 0, 'RF01'),
+(14, 'BeAT eSP CW Dance White', 'Rp 15,305,000', 'Motor Honda', 1, 'BE02'),
+(15, 'CB150 Verza CW Masculine Black', 'Rp 19,605,000', 'Motor Honda', 4, 'CB03');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pesan`
+--
+
+CREATE TABLE `pesan` (
+  `id` int(50) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `message` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `pesan`
+--
+
+INSERT INTO `pesan` (`id`, `name`, `email`, `message`) VALUES
+(1, 'Achmad Imannudin', 'imantech@merahputih.id', 'Hello World!');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(25) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `phone` int(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `email`, `password`, `phone`) VALUES
+(1, 'iman', 'iman@gmail.com', '5be9a68073f66a56554e25614e9f1c9a', 123);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `verifikasi`
+--
+
+CREATE TABLE `verifikasi` (
+  `id` int(25) NOT NULL,
+  `nama` varchar(100) DEFAULT NULL,
+  `nama_produk` varchar(255) DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL,
+  `metode` varchar(255) NOT NULL,
+  `waktu` varchar(255) NOT NULL,
+  `jenis` varchar(255) NOT NULL,
+  `qty` int(11) NOT NULL,
+  `kode_barang` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `verifikasi`
+--
+
+INSERT INTO `verifikasi` (`id`, `nama`, `nama_produk`, `status`, `metode`, `waktu`, `jenis`, `qty`, `kode_barang`) VALUES
+(46, 'iman', 'CB150 Verza CW Masculine Black', 'Diterima', 'Credit', 'ATM', '1 Tahun', 1, 'BE02'),
+(47, 'iman', 'BeAT eSP CW Dance White', 'Diterima', 'Cash', 'Cash', '2 Hari', 1, 'BE02'),
+(48, 'iman', 'CB150 Verza CW Masculine Black', 'Menunggu Persetujuan', 'Credit', 'Credit Card', '2 Bulan', 1, 'CB03'),
+(49, 'iman', 'Revo FIT Galaxy Blue', 'Ditolak', 'Credit', 'ATM', '2 Bulan', 1, 'RF01');
+
+--
+-- Triggers `verifikasi`
+--
+DELIMITER $$
+CREATE TRIGGER `TG_Verif` AFTER INSERT ON `verifikasi` FOR EACH ROW BEGIN
+UPDATE barang SET stok = stok - NEW.qty WHERE kode_barang = NEW.kode_barang;
+END
+$$
+DELIMITER ;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `nama_produk` (`nama_produk`),
+  ADD KEY `kategori` (`kategori`),
+  ADD KEY `kode_barang` (`kode_barang`);
+
+--
+-- Indexes for table `pesan`
+--
+ALTER TABLE `pesan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `username` (`username`);
+
+--
+-- Indexes for table `verifikasi`
+--
+ALTER TABLE `verifikasi`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `nama` (`nama`),
+  ADD KEY `nama_produk` (`nama_produk`),
+  ADD KEY `kode_barang` (`kode_barang`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(25) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `barang`
+--
+ALTER TABLE `barang`
+  MODIFY `id` int(25) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `pesan`
+--
+ALTER TABLE `pesan`
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(25) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `verifikasi`
+--
+ALTER TABLE `verifikasi`
+  MODIFY `id` int(25) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `verifikasi`
+--
+ALTER TABLE `verifikasi`
+  ADD CONSTRAINT `verifikasi_ibfk_1` FOREIGN KEY (`nama`) REFERENCES `user` (`username`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `verifikasi_ibfk_2` FOREIGN KEY (`nama_produk`) REFERENCES `barang` (`nama_produk`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `verifikasi_ibfk_3` FOREIGN KEY (`kode_barang`) REFERENCES `barang` (`kode_barang`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
